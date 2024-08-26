0efe274d3d0977880144c498d075a6832d1e4761
cristhina23 <viszellechacon@gmail.com>
2024-08-26 11:49:53 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      display: ['Open Sans', 'sans-serif'],
+      body: ['Open Sans', 'sans-serif'],
+    },
+    extend: {
+      fontSize: {
+        14: '14px',
+      },
+      backgroundColor: {
+        'main-bg': '#FAFBFB',
+        'main-dark-bg': '#20232A',
+        'secondary-dark-bg': '#33373E',
+        'light-gray': '#F7F7F7',
+        'half-transparent': 'rgba(0, 0, 0, 0.5)',
+      },
+      borderWidth: {
+        1: '1px',
+      },
+      borderColor: {
+        color: 'rgba(0, 0, 0, 0.1)',
+      },
+      width: {
+        400: '400px',
+        760: '760px',
+        780: '780px',
+        800: '800px',
+        1000: '1000px',
+        1200: '1200px',
+        1400: '1400px',
+      },
+      height: {
+        80: '80px',
+      },
+      minHeight: {
+        590: '590px',
+      },
+      backgroundImage: {
+        'hero-pattern':
+          "url('https://img.freepik.com/vector-gratis/palabra-gran-venta-carrito-compras-cajas-regalo-tienda-mapa-puntero-estilo-realista-ilustracion-vectorial_548887-126.jpg?w=740&t=st=1681708122~exp=1681708722~hmac=9bc5b8ab86a49c605678dffca0f48f7155370625b9180efce8c86b59866b4179')",
+      },
+    },
+  },
+  plugins: [],
+};
+/*https://img.freepik.com/vector-gratis/inspeccion-producto-concepto-doodle-control-calidad_107791-10787.jpg?w=900&t=st=1681707833~exp=1681708433~hmac=657e6ba5251f37c0c53ef6cffdb2d538c3ff926499c5c0908a0caf8103210c91
+
+/*https://img.freepik.com/psd-premium/concepto-tienda-compras-linea-telefono-movil-carrito-compras-3d-como-icono_106244-2052.jpg?w=740*/
+
